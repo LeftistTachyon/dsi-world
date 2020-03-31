3c277e61139b35b30bc22458b12d7a36bfbe863c
Gui Guy <jedandeganwang1999@gmail.com>
2020-03-31 10:49:23 -0500

    Initial commit'

---
+++ src/test/java/com/github/leftisttachyon/dsihub/DsiHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.leftisttachyon.dsihub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsiHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
