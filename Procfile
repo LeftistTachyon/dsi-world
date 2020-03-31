978a3fbe22c64093e90fb081cd138c78ed0eeb9e
Gui Guy <jedandeganwang1999@gmail.com>
2020-03-31 12:15:10 -0500

    I did a comment

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 # maven version: ./mvnw clean install exec:java
-web: java -jar -Dserver.port=$PORT target/dsi-hub-1.0.0-SNAPSHOT.jar
+web: java -jar -Dserver.port=$PORT target/dsi-hub-0.0.1-SNAPSHOT.jar
--- src/main/java/com/github/leftisttachyon/dsihub/DsiHubApplication.java
+++ src/main/java/com/github/leftisttachyon/dsihub/DsiHubApplication.java
@@ -3,11 +3,22 @@ package com.github.leftisttachyon.dsihub;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The main class for this application.
+ *
+ * @author Jed Wang
+ * @since 0.0.1
+ */
 @SpringBootApplication
 public class DsiHubApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DsiHubApplication.class, args);
-	}
+    /**
+     * The main method
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(DsiHubApplication.class, args);
+    }
 
 }
