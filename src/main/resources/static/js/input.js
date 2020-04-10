938974c63dabdb36b48c5876d6cd0f74fbb90eca
Gui Guy <jedandeganwang1999@gmail.com>
2020-04-10 14:42:11 -0500

    AAAAAAAAAAAAAAAA

--- src/main/resources/static/js/input.js
+++ src/main/resources/static/js/input.js
@@ -1,41 +1,46 @@
-$(document).ready(function() {
-var canvas = document.getElementById("bottom-canvas");
-var ctx = canvas.getContext("2d");
+try{
+    $(document).ready(function() {
+        alert("Document is ready");
+        var canvas = document.getElementById("bottom-canvas");
+        var ctx = canvas.getContext("2d");
 
-ctx.font = "15px monospace";
+        ctx.font = "15px monospace";
 
-function clearScreen() {
-    ctx.fillStyle = "#fff";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-}
-
-var previousY = 0, previousDY = 0, scrollCnt = 0, keyStr = "";
-
-document.body.addEventListener("scroll", function() {
-    clearScreen();
-
-    ctx.fillStyle = "#000";
-    ctx.fillText(window.scrollY, 10, 30);
-
-    var currentDY = window.scrollY - previousY;
-    ctx.fillText(currentDY, 10, 50);
-
-    ctx.fillText(++scrollCnt, 10, 70);
-
-    previousY = window.scrollY;
-
-    var mag = Math.abs(currentDY); // magnitude of currentDY
-    console.log(mag);
-    if(mag > Math.abs(previousDY)) {
-        if(currentDY > 0) {
-            keyStr = mag + " down";
-        } else {
-            keyStr = mag + " up";
+        function clearScreen() {
+            ctx.fillStyle = "#fff";
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
         }
-    }
 
-    ctx.fillText(keyStr, 10, 100);
+        var previousY = 0, previousDY = 0, scrollCnt = 0, keyStr = "";
 
-    previousDY = currentDY;
-}, false);
-});
+        document.body.addEventListener("scroll", function() {
+            clearScreen();
+
+            ctx.fillStyle = "#000";
+            ctx.fillText(window.scrollY, 10, 30 + window.scrollY);
+
+            var currentDY = window.scrollY - previousY;
+            ctx.fillText(currentDY, 10, 50 + window.scrollY);
+
+            ctx.fillText(++scrollCnt, 10, 70 + window.scrollY);
+
+            previousY = window.scrollY;
+
+            var mag = Math.abs(currentDY); // magnitude of currentDY
+            console.log(mag);
+            if(mag > Math.abs(previousDY)) {
+                if(currentDY > 0) {
+                    keyStr = mag + " down";
+                } else {
+                    keyStr = mag + " up";
+                }
+            }
+
+            ctx.fillText(keyStr, 10, 100 + window.scrollY);
+
+            previousDY = currentDY;
+        }, false);
+    });
+} catch(e) {
+    alert(e);
+}
