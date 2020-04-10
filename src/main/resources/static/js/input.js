7bccd7c55b41e3e167338041da8065b9c78d3533
Gui Guy <jedandeganwang1999@gmail.com>
2020-04-10 17:36:37 -0500

    ALKJFLDKJFSKSFJGHFA

--- src/main/resources/static/js/input.js
+++ src/main/resources/static/js/input.js
@@ -10,14 +10,16 @@ window.onload = function() {
     var previousY = 0, previousDY = 0, scrollCnt = 0, keyStr = '';
 
     document.addEventListener('scroll', function() {
+        alert(ctx.fillText);
+
         var topY = window.scrollY || document.body.scrollTop;
 
         clearScreen();
 
         ctx.fillStyle = '#000';
         ctx.fillText(topY, 10, 30 + topY);
-        try {
 
+        try {
             var currentDY = topY - previousY;
             ctx.fillText(currentDY, 10, 50 + topY);
 
