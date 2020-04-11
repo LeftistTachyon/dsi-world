1ccf8879d279ab32010a43bd91c252769ecdb13a
Gui Guy <jedandeganwang1999@gmail.com>
2020-04-10 22:29:23 -0500

    I hope this pixel number works

--- src/main/resources/static/js/input.js
+++ src/main/resources/static/js/input.js
@@ -1,14 +1,78 @@
+CanvasRenderingContext2D.prototype.pixelNums = {
+    "0" :   [[1, 1, 1],
+             [1, 0, 1],
+             [1, 0, 1],
+             [1, 0, 1],
+             [1, 1, 1]],
+    "1" :   [[0, 1, 0],
+             [1, 1, 0],
+             [0, 1, 0],
+             [0, 1, 0],
+             [1, 1, 1]],
+    "2" :   [[1, 1, 1],
+             [0, 0, 1],
+             [1, 1, 1],
+             [1, 0, 0],
+             [1, 1, 1]],
+    "3" :   [[1, 1, 1],
+             [0, 0, 1],
+             [1, 1, 1],
+             [0, 0, 1],
+             [1, 1, 1]],
+    "4" :   [[1, 0, 1],
+             [1, 0, 1],
+             [1, 1, 1],
+             [0, 0, 1],
+             [0, 0, 1]],
+    "5" :   [[1, 1, 1],
+             [1, 0, 0],
+             [1, 1, 0],
+             [0, 0, 1],
+             [1, 1, 0]],
+    "6" :   [[1, 1, 1],
+             [1, 0, 0],
+             [1, 1, 1],
+             [1, 0, 1],
+             [1, 1, 1]],
+    "7" :   [[1, 1, 1],
+             [0, 0, 1],
+             [0, 0, 1],
+             [0, 0, 1],
+             [0, 0, 1]],
+    "8" :   [[1, 1, 1],
+             [1, 0, 1],
+             [1, 1, 1],
+             [1, 0, 1],
+             [1, 1, 1]],
+    "9" :   [[1, 1, 1],
+             [1, 0, 1],
+             [1, 1, 1],
+             [0, 0, 1],
+             [1, 1, 1]],
+    "-" :   [[0, 0, 0],
+             [0, 0, 0],
+             [1, 1, 1],
+             [0, 0, 0],
+             [0, 0, 0]]
+};
+
 CanvasRenderingContext2D.prototype.fillNumber = function(num, x, y) {
-    num |= 0;
-    for(var i = 0; num > 0; i++) {
-        var digit = num % 10;
-        var r = digit.toString(16).padStart(2, '0');
+    var str = "" + Math.round(num, 2);
+    for(var i = 0; i < str.length; i++) {
+        var mat = ctx.pixelNums[str.charAt(i)];
+        if(typeof mat === 'undefined') {
+            console.log("Couldn't find " + str.charAt(i) + ", continuing");
+            continue;
+        }
 
-        ctx.fillStyle = "#" + r + r + r;
-
-        ctx.fillRect(x + i * 5, y - 10, 5, 10);
-
-        num = (num / 10) | 0;
+        for(var j = 0, y_ = y - 15; j < mat.length; j++, y_ += 3) {
+            var arr = mat[j];
+            for(var k = 0, x_ = x + i * 12; k < arr.length; k++, x_ += 3) {
+                if(arr[k]) {
+                    ctx.fillRect(x_, y_, 3, 3);
+                }
+            }
+        }
     }
 };
 
@@ -21,11 +85,9 @@ window.onload = function() {
         ctx.fillRect(0, 0, canvas.width, canvas.height);
     }
 
-    var previousY = 0, previousDY = 0, scrollCnt = 0, keyStr = 0;
+    var previousY = 0, previousDY = 0, scrollCnt = 0, key = 0;
 
     document.addEventListener('scroll', function() {
-        alert(ctx.fillNumber);
-
         var topY = window.scrollY || document.body.scrollTop;
 
         clearScreen();
@@ -44,14 +106,12 @@ window.onload = function() {
             var mag = Math.abs(currentDY); // magnitude of currentDY
             // console.log(mag);
             if(mag > Math.abs(previousDY)) {
-                if(currentDY > 0) {
-                    keyStr = mag;
-                } else {
-                    keyStr = mag;
-                }
+                key = mag;
             }
 
-            ctx.fillNumber(keyStr, 10, 100 + topY);
+            ctx.fillRect(10, 77 + topY, 80, 1);
+
+            ctx.fillNumber(key, 10, 100 + topY);
 
             previousDY = currentDY;
         } catch(e) {
