d4645b9c4ec1d3f804d1888da0d3f0b4413b2408
Gui Guy <jedandeganwang1999@gmail.com>
2020-04-17 18:36:00 -0500

    Change API slightly

--- src/main/resources/static/js/fillNumber.js
+++ src/main/resources/static/js/fillNumber.js
@@ -1,4 +1,4 @@
-CanvasRenderingContext2D.prototype.pixelNums = {
+CanvasRenderingContext2D.prototype.pixelChars = {
     "0" :   [[1, 1, 1],
              [1, 0, 1],
              [1, 0, 1],
@@ -53,25 +53,37 @@ CanvasRenderingContext2D.prototype.pixelNums = {
              [0, 0, 0],
              [1, 1, 1],
              [0, 0, 0],
-             [0, 0, 0]]
+             [0, 0, 0]],
+    "." :   [[0, 0, 0],
+             [0, 0, 0],
+             [0, 0, 0],
+             [0, 0, 0],
+             [0, 1, 0]]
 };
 
 CanvasRenderingContext2D.prototype.fillNumber = function(num, x, y) {
     var str = "" + Math.round(num);
+    this.fillPixelText(str, x, y);
+};
+
+CanvasRenderingContext2D.prototype.fillPixelText = function(str, x, y) {
+    var baseX = x;
     for(var i = 0; i < str.length; i++) {
-        var mat = ctx.pixelNums[str.charAt(i)];
+        var mat = ctx.pixelChars[str.charAt(i)];
         if(typeof mat === 'undefined') {
             console.log("Couldn't find " + str.charAt(i) + ", continuing");
             continue;
         }
 
-        for(var j = 0, y_ = y - 10; j < mat.length; j++, y_ += 2) {
+        for(var j = 0, y_ = y - 2 * mat.length; j < mat.length; j++, y_ += 2) {
             var arr = mat[j];
-            for(var k = 0, x_ = x + i * 8; k < arr.length; k++, x_ += 2) {
+            for(var k = 0, x_ = baseX; k < arr.length; k++, x_ += 2) {
                 if(arr[k]) {
                     ctx.fillRect(x_, y_, 2, 2);
                 }
             }
         }
+
+        baseX += 2 * (mat[0].length + 1);
     }
-};
+}
