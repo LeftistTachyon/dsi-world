ae4792ad172bd269ecc9b2130b5e3ebee54adaa9
Gui Guy <jedandeganwang1999@gmail.com>
2020-04-29 16:15:44 -0500

    Hopefully this should work now

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # DSi World
-A Spring-backed website created for use by the DSi.
+A Spring-backed website created for use by the DSi.  
+Right now, there's not much on there, but I plan to put a lot of cool things on here at point or another.
+
+## Cool things in the works  
+These are things that either people have suggested or I thought might be cool to have on a DSi.
+ - DSi-compatible email client
+ - Git-like command line with in-built editor
+ - Manga reader
--- src/main/resources/static/js/expandable.js
+++ src/main/resources/static/js/expandable.js
@@ -4,19 +4,25 @@ try {
     Array.prototype.slice.call(document.getElementsByClassName("expandable"))
             .forEach(function(item, idx){
         item.addEventListener("click", function() {
-            // console.log("clicked " + idx);
-            var content = contents[idx];
-            // console.log(content + " " + content.style.display);
-            if (content.style.display) {
-                this.setAttribute("data-sign", "-");
-                content.style.display = null;
-            } else {
-                this.setAttribute("data-sign", "+");
-                content.style.display = "none";
+            // alert(this + " clicked")
+
+            try {
+                // console.log("clicked " + idx);
+                var content = contents[idx];
+                // alert(content + " " + content.style.display);
+                if (content.style.display == "none") {
+                    this.setAttribute("data-sign", "-");
+                    content.style.display = "block";
+                } else {
+                    this.setAttribute("data-sign", "+");
+                    content.style.display = "none";
+                }
+            } catch(e) {
+                alert(e);
             }
 
             return false;
-        });
+        }, false);
 
         if(!item.getAttribute("data-sign"))
             item.setAttribute("data-sign", "+");
