4e066a94fb34e8ddfd1f6a1fd13f25c6114268b5
Gui Guy <jedandeganwang1999@gmail.com>
2020-04-30 22:35:22 -0500

    When in doubt, alert it all

--- src/main/resources/static/js/expandable.js
+++ src/main/resources/static/js/expandable.js
@@ -1,8 +1,16 @@
 try {
     var contents = document.getElementsByClassName("expand-content");
 
+    alert(Array);
+    alert(Array.prototype);
+    alert(Array.prototype.slice);
+    alert(Array.prototype.slice.call);
+    alert(Array.prototype.forEach);
+
     Array.prototype.slice.call(document.getElementsByClassName("expandable"))
             .forEach(function(item, idx){
+        alert("starting " + idx);
+
         item.addEventListener("click", function() {
             // alert(this + " clicked")
 
@@ -26,6 +34,8 @@ try {
 
         if(!item.getAttribute("data-sign"))
             item.setAttribute("data-sign", "+");
+
+        alert("done " + idx);
     });
 } catch(err) {
     alert(err);
