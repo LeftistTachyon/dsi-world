b7ca644f51f8ac71e919e544a48d4500f2507226
Gui Guy <jedandeganwang1999@gmail.com>
2020-05-26 14:58:41 -0500

    Help

--- src/main/java/com/github/leftisttachyon/dsiworld/config/SecurityConfig.java
+++ src/main/java/com/github/leftisttachyon/dsiworld/config/SecurityConfig.java
@@ -1,12 +1,13 @@
 package com.github.leftisttachyon.dsiworld.config;
 
-import com.github.leftisttachyon.dsiworld.data.UserList;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -14,12 +15,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     /**
      * The list of users associated with this application
      */
-    @Autowired
-    private UserList userList;
+//    @Autowired
+//    private UserList userList;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userList);
+//        auth.userDetailsService(userList);
+        auth.inMemoryAuthentication()
+                .withUser("user1").password(passwordEncoder().encode("user1Pass")).roles("USER");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Override
